1e590931c2187310816f491562ceba1a7abdbcd2
MOON_CLJ <lijunli2598@gmail.com>
2013-07-28 14:59:41 +0800

    init

---
+++ learning_cpp.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/*
+int main()
+{
+    int sum = 0, value;
+    while (std::cin >> value)
+        sum += value;
+    std::cout << sum << std::endl;
+    return 0;
+}
+*/
+
+class Foo {
+};
+
+int main() {
+    return 0;
+}
