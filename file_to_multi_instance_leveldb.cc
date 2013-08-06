0580ac274920c4bd2853349d358a415ecbefb04b
MOON_CLJ <lijunli2598@gmail.com>
2013-08-06 10:44:30 +0800

    准备引入boost

--- file_to_multi_instance_leveldb.cc
+++ file_to_multi_instance_leveldb.cc
@@ -3,11 +3,14 @@
 #include <sstream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include "leveldb/db.h"
 #include "leveldb/write_batch.h"
 #include "leveldb/comparator.h"
+#include "boost/filesystem.hpp"
 
 using std::cerr;
 using std::cout;
@@ -26,20 +29,35 @@ int str2int(const std::string &str) {
   return n;
 }
 
-int main(int argc, char** argv) {
+int divide_to_multi_db(leveldb::DB*& db) {
   // const params
   const int single_instance_size = 200000;
+  leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
 
-  std::string multi_instance_ldb_dir;
-  if (argc > 1) {
-    // TODO
-    multi_instance_ldb_dir = argv[1];
-  } else {
-    multi_instance_ldb_dir = "/tmp/test_file_to_multi_instance_leveldb/1";
+  for (it->SeekToFirst(); it->Valid(); it->Next())
+  {
+      std::cout << it->key().ToString() << " : " << it->value().ToString() << std::endl;
   }
 
+  return 0;
+}
+
+void mk_dir_if(const std::string& dir) {
+  cout << dir << endl;
+  int mkdir_status = mkdir(dir.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+  cout << mkdir_status << endl;
+  cout << errno << endl;
+}
+
+int main(int argc, char** argv) {
+  std::string ldb_base_dir = "/tmp/test_file_to_multi_instance_leveldb";
+  std::string current_collection = "global_activity";
+  std::vector<std::string> multi_instance_dirs;
+
+  mk_dir_if(ldb_base_dir + "/" + current_collection);
+  /*
   std::string multi_instance_ldb_base_dir = multi_instance_ldb_dir.substr(0, multi_instance_ldb_dir.rfind('/'));
-  mkdir(multi_instance_ldb_base_dir.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+  int mkdir_status = mkdir(multi_instance_ldb_base_dir.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 
   // leveldb config
   leveldb::Options options;
@@ -65,12 +83,15 @@ int main(int argc, char** argv) {
     key = line.substr(0, delimiter_idx);
     value = line.substr(delimiter_idx + 1);
     batch.Put(key, value);
+    break;
   }
   status = db->Write(leveldb::WriteOptions(), &batch);
   assert(status.ok());
 
+  divide_to_multi_db(db);
   infile.close();
   delete db;
+  */
 
   return 0;
 }
