a190ec583c4c679ae6faf8c176e4ea27dcdc458f
MOON_CLJ <lijunli2598@gmail.com>
2015-02-11 17:42:13 +0800

    fix

--- algorithm/insertionSort.cpp
+++ algorithm/insertionSort.cpp
@@ -2,14 +2,22 @@
 #include "insertionSort.h"
 
 void insertionSort(int* l, size_t len) {
-    int i, j, min, tmp;
-    for (i = 1; i < len; i++) {
-        min = i;
-        for (j = i - 1; j >= 0 && l[j] > l[i]; j--)
-            min = j;
-        tmp = l[i];
-        for (j = i; j > min; j--)
-            l[j] = l[j - 1];
-        l[min] = tmp;
+    size_t i, j, one_next;
+    int tmp;
+    for (i = 1; i < len; ++i) {
+        one_next = i;
+        j = i - 1;
+        while (l[j] > l[i]) {
+            one_next = j;
+            if (one_next == 0)
+                break;
+            --one_next;
+        }
+        if (one_next != i) {
+            tmp = l[i];
+            for (j = i; j > one_next; j--)
+                l[j] = l[j - 1];
+            l[one_next] = tmp;
+        }
     }
 }
